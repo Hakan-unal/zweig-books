a240ff7122ea66b877a9c6ae04a524d9bc559997
Hakan Ünal <hakanunal_1996@hotmail.com>
2023-04-12 01:40:51 +0300

    demo updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,52 +1,160 @@
 
-import { useEffect, useState } from 'react';
-import { Select, Table, Row, Col, Button } from "antd"
-import { AiFillEye, AiFillEyeInvisible } from "react-icons/ai";
-import { useLocalStorage } from './hooks/useLocalStorage';
-import tableColumsFile from "./staticData/tableColumsFile.json"
-import tableColumsData from "./staticData/tableColumnsData.json"
+import { useState, useRef } from 'react';
+import { Table, Row, Col, Image, Button, Input, Space } from "antd"
+import books from "./staticData/books.json"
+import { AiFillEye, AiOutlineMonitor } from "react-icons/ai";
+import type { InputRef } from 'antd';
+import type { ColumnType } from 'antd/es/table';
+import type { FilterConfirmProps } from 'antd/es/table/interface';
+import Highlighter from 'react-highlight-words';
 
+
+
+
+interface DataType {
+  image: string;
+  name: string;
+  year: string;
+  description: string;
+  sell: string;
+}
+type DataIndex = keyof DataType;
 
 
 const App = () => {
-  const [loading, setLoading] = useState(true)
-  const [data, setData] = useState([])
-
-  const [isShowTableSorter, setIsShowTableSorter] = useState(true)
-
-
-
-
-
-  const handleChange = (value: any) => {
-    const tempArr: any = []
-    value.map((index: any) => tempArr.push(tableColumsFile[index]))
-
+  const [data, setData] = useState(books)
+  const [searchText, setSearchText] = useState('');
+  const [searchedColumn, setSearchedColumn] = useState('');
+  const searchInput = useRef<InputRef>(null);
+  const handleSearch = (
+    selectedKeys: string[],
+    confirm: (param?: FilterConfirmProps) => void,
+    dataIndex: DataIndex,
+  ) => {
+    confirm();
+    setSearchText(selectedKeys[0]);
+    setSearchedColumn(dataIndex);
   };
 
-  const handleGetData = () => {
-    setData([])
-  }
+  const handleReset = (clearFilters: () => void) => {
+    clearFilters();
+    setSearchText('');
+  };
+
+  const getColumnSearchProps = (dataIndex: DataIndex): ColumnType<DataType> => ({
+    filterDropdown: ({ setSelectedKeys, selectedKeys, confirm, clearFilters, close }) => (
+      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
+        <Input
+          ref={searchInput}
+          placeholder={`Search ${dataIndex}`}
+          value={selectedKeys[0]}
+          onChange={(e) => setSelectedKeys(e.target.value ? [e.target.value] : [])}
+          onPressEnter={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+          style={{ marginBottom: 8, display: 'block' }}
+        />
+        <Space>
+          <Button
+            type="primary"
+            onClick={() => handleSearch(selectedKeys as string[], confirm, dataIndex)}
+            icon={<AiOutlineMonitor />}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={() => clearFilters && handleReset(clearFilters)}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Reset
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              confirm({ closeDropdown: false });
+              setSearchText((selectedKeys as string[])[0]);
+              setSearchedColumn(dataIndex);
+            }}
+          >
+            Filter
+          </Button>
+          <Button
+            type="link"
+            size="small"
+            onClick={() => {
+              close();
+            }}
+          >
+            close
+          </Button>
+        </Space>
+      </div>
+    ),
+    filterIcon: (filtered: boolean) => (
+      <AiOutlineMonitor size={25} style={{ color: filtered ? '#1890ff' : undefined }} />
+    ),
+    onFilter: (value, record) =>
+      record[dataIndex]
+        .toString()
+        .toLowerCase()
+        .includes((value as string).toLowerCase()),
+    onFilterDropdownOpenChange: (visible) => {
+      if (visible) {
+        setTimeout(() => searchInput.current?.select(), 100);
+      }
+    },
+    render: (text) =>
+      searchedColumn === dataIndex ? (
+        <Highlighter
+          highlightStyle={{ backgroundColor: '#ffc069', padding: 0 }}
+          searchWords={[searchText]}
+          autoEscape
+          textToHighlight={text ? text.toString() : ''}
+        />
+      ) : (
+        text
+      ),
+  });
 
 
+  const columns = [
+    {
+      title: 'Görsel',
+      dataIndex: 'image',
+      key: 'image',
+      render: (image: string) => <Image width={100} src={image} />
+    },
+    {
+      title: 'Ad',
+      dataIndex: 'name',
+      key: 'name',
+      ...getColumnSearchProps('name'),
+    },
+    {
+      title: 'Basım Yılı',
+      dataIndex: 'year',
+      key: 'year',
+    },
+    {
+      title: 'Açıklama',
+      dataIndex: 'description',
+      key: 'description',
+    },
+    {
+      title: 'Ön İzleme',
+      dataIndex: 'sell',
+      key: 'sell',
+      render: (sell: string) => <Button href={sell} ><AiFillEye size={20} /></Button>
 
-  useEffect(() => {
-
-  }, [])
-
-  useEffect(() => {
-    setTimeout(() => {
-      setLoading(!loading)
-      handleGetData()
-    }, 5000)
-  }, [])
-
-
+    },
+  ];
   return (<Row style={{ marginTop: 100 }}>
 
 
     <Col sm={{ span: 16, offset: 4 }}>
-      <Table loading={loading} columns={[]} dataSource={data} />
+      <Table columns={columns} dataSource={data} />
     </Col>
   </Row>)
 }
